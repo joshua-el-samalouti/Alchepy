3beb7f6eeed50433a059d282f10f87bd14d66267
Joshua El-Samalouti <joshua@el-samalouti.de>
2024-08-25 15:51:40 +0200

    0.0.1.2
    - Module.match_elements method was finished, but is not yet tested and lacks debug info

--- alchlib/module.py
+++ alchlib/module.py
@@ -102,7 +102,7 @@ class Module:
             print("Recipe #",recipe_id,": ",self.recipes[recipe_id])
         return self.recipes[recipe_id]
 
-    # checks if the given element is discovered or not
+    # checks if the given element is valid and discovered or not
     def is_unlocked_element(self, element: str):
         subset = self.elements.loc[self.elements[2] == 1].loc[self.elements[0] == element]
         if subset.empty:
@@ -128,27 +128,46 @@ class Module:
             return False
 
     # TODO: ADD DEBUG MESSAGES
-    # TODO: FINISH METHOD
     # TODO: REFACTOR OUTPUT TO BE MORE INTUITIVE
     # checks if given elements match and unlocks recipe and result if they do
-    # returns [-1,-1,[-1]] if elements are invalid or locked
-    # returns [0,-1,[-1]] if elements don't match
-    # returns [1, recipe_id,[-1]] if elements match, but the recipe was discovered already
-    # returns [2, recipe_id, [-1]] if elements match, the results were discovered already,
+    # returns [-1,-1,[]] if elements are invalid or locked
+    # returns [0,-1,[]] if elements don't match
+    # returns [1, recipe_id,[]] if elements match, but the recipe was discovered already
+    # returns [2, recipe_id, []] if elements match, the results were discovered already,
     # but the recipe is new
     # returns [3, recipe_id,[new_elements]] if elements match and there is one or more
     # new elements unlocked
+    # the format is [status, recipe id, [unlocked elements]]
     def match_elements(self, element_1: str, element_2: str):
         unlocked_1 = self.is_unlocked_element(element_1)
         unlocked_2 = self.is_unlocked_element(element_2)
         if unlocked_1 and unlocked_2:
             recipe_id = self.find_recipe(element_1, element_2)
             if recipe_id >= 0:
-                # TODO: what happens when elements match
-                return 1
+                # elements match
+                recipe = self.get_recipe(recipe_id)
+                if recipe[3] == 1:
+                    # the recipe was discovered already
+                    return [1, recipe_id, []]
+                else:
+                    # the recipe was not discovered yet
+                    self.unlock_recipe(recipe_id)
+                    newly_unlocked = []
+                    for element in recipe[2]:
+                        if not self.is_unlocked_element(element):
+                            newly_unlocked.append(element)
+                            self.unlock_element(element)
+                    if len(newly_unlocked) == 0:
+                        # there are no newly unlocked elements
+                        return [2, recipe_id, []]
+                    else:
+                        # there are newly unlocked elements
+                        return [3, recipe_id, newly_unlocked]
             else:
-                # TODO: what happens when elements don't match
-                return 0
+                # elements don't match
+                return [0, -1, []]
         else:
-            # TODO: what happens when given elements are invalid or locked
-            return -1
+            # one or more elements are invalid or locked
+            # no information about which element is invalid is returned
+            # for information about this other methods have to be used additionally
+            return [-1, -1, []]
