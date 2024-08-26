afc4bb0f64b2b815e31a3f1f7e3f7ec8a220fb10
Joshua El-Samalouti <joshua@el-samalouti.de>
2024-08-26 23:31:28 +0200

    Update start_console_app.py

--- start_console_app.py
+++ start_console_app.py
@@ -2,4 +2,6 @@ import alchlib.console_app
 
 path_desktop = "C:/Users/joshu/Documents/Github/alchemy-modules/Covers/doodle_mafia.json"
 path_laptop = "C:/Users/maeba/OneDrive/Desktop/Alchemy Modules/alchemy-modules/Covers/doodle_mafia.json"
-alchlib.console_app.start_module(path_laptop)
+print("please enter the path of the module (json file) you want to play:")
+path = input()
+alchlib.console_app.start_module(path)
